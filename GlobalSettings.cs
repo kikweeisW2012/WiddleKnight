c48ea860a9be9816f33c689a2938eaa24762621f
kikwee <kikwee2012@gmail.com>
2025-12-08 09:58:38 -0500

    Delete GlobalSettings.cs

--- GlobalSettings.cs
+++
@@ -1,8 +0,0 @@
-namespace WiddleKnight
-{
-    public class GlobalSettings
-    {
-        public int SelectedSkinOption { get; set; } = 0;
-        public int CustomSubOption { get; set; } = 0;
-    }
-}
